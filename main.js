dfe44385805cceb84319c7f552e8df7784df1595
Θαλησ <104565512+Thalesed@users.noreply.github.com>
2023-05-11 10:30:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,16 +3,20 @@ const balloon = document.querySelector('.balloon');
 
 trigger.addEventListener('mouseover', () => {
   balloon.style.display = 'block';
+  trigger.style.display = 'none';
 });
 
 trigger.addEventListener('mouseout', () => {
   balloon.style.display = 'none';
+  trigger.style.display = 'flex';
 });
 
 balloon.addEventListener('mouseover', () => {
     balloon.style.display = 'block';
+    trigger.style.display = 'none';
   });
 
 balloon.addEventListener('mouseout', () => {
     balloon.style.display = 'none';
+    trigger.style.display = 'flex';
   });
